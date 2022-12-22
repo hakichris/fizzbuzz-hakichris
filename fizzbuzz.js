565fd4f8b98271378526d5914ebb3f4c11ea5fe4
Christian Hakizimana <hakizachrist1@gmail.com>
2022-12-22 15:40:39 +0200

    Uploaded Files for challenge

---
+++ fizzbuzz.js
@@ -0,0 +1,24 @@
+module.exports = function () {
+  // write your code here
+  let arr= [];
+  
+  for (let i = 1; i <101; i++){
+  if (i%3===0 && i%5===0){
+    arr.push('FizzBuzz');
+    }
+    
+  else if (i%3===0){
+    arr.push('Fizz');
+    }
+    
+  else if (i%5===0){
+    arr.push('Buzz');
+    }
+   
+  else {
+    arr.push(i);
+    }
+  }
+  
+  return arr; 
+}
